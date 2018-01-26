be4e182069d6764c258d7085e8acbbafa1a4dee0
ezhov-da <ezhdenis@yandex.ru>
2018-01-26 05:54:19 +0300

    Add dependency

--- src/main/java/ru/ezhov/logging/servlets/SrvltLogger.java
+++ src/main/java/ru/ezhov/logging/servlets/SrvltLogger.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+
 import ru.ezhov.logging.beans.Errors;
 import ru.ezhov.logging.beans.Log;
 import ru.ezhov.logging.db.LogTreatment;
@@ -20,25 +21,22 @@ import ru.ezhov.logging.xml.LogUnmarshaller;
 /**
  * Сервлет, который отвечает за логирование в БД
  * <p>
+ *
  * @author ezhov_da
  */
-public class SrvltLogger extends HttpServlet
-{
+public class SrvltLogger extends HttpServlet {
     private static final Logger LOG = Logger.getLogger(SrvltLogger.class.getName());
     private HttpServletRequest request;
     private HttpServletResponse response;
 
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         this.request = request;
         this.response = response;
         request.setCharacterEncoding("UTF-8");
-        try (PrintWriter out = response.getWriter())
-        {
-            try
-            {
+        try (PrintWriter out = response.getWriter()) {
+            try {
                 LOG.info(request.getContentType());
 
                 String xmlText = getXmlTextFromRequest();
@@ -53,25 +51,20 @@ public class SrvltLogger extends HttpServlet
                 String string = treatment.treatment(query);
 
                 response.setContentType("text/xml;charset=UTF-8");
-                if ("".equals(string))
-                {
+                if ("".equals(string)) {
                     response.setStatus(HttpServletResponse.SC_OK);
-                } else
-                {
+                } else {
                     out.print(string);
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 }
 
-            } catch (Exception ex)
-            {
-                try
-                {
+            } catch (Exception ex) {
+                try {
                     String s = getExceptionMsg(ex);
                     out.print(s);
                     LOG.log(Level.SEVERE, "Ошибка обработки.", ex);
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                } catch (Exception ex1)
-                {
+                } catch (Exception ex1) {
                     LOG.log(Level.SEVERE, null, ex1);
                 }
 
@@ -80,13 +73,10 @@ public class SrvltLogger extends HttpServlet
     }
 
 
-    private String getXmlTextFromRequest() throws IOException
-    {
-        try (Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");)
-        {
+    private String getXmlTextFromRequest() throws IOException {
+        try (Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");) {
             StringBuilder stringBuider = new StringBuilder();
-            while (scanner.hasNextLine())
-            {
+            while (scanner.hasNextLine()) {
                 stringBuider.append(scanner.nextLine());
             }
 
@@ -95,12 +85,9 @@ public class SrvltLogger extends HttpServlet
         }
     }
 
-    private String getExceptionMsg(Exception exception) throws Exception
-    {
-        try (StringWriter stringWriter = new StringWriter();)
-        {
-            try (PrintWriter printWriter = new PrintWriter(stringWriter);)
-            {
+    private String getExceptionMsg(Exception exception) throws Exception {
+        try (StringWriter stringWriter = new StringWriter();) {
+            try (PrintWriter printWriter = new PrintWriter(stringWriter);) {
                 exception.printStackTrace(printWriter);
 
                 String error = stringWriter.toString();
@@ -109,8 +96,7 @@ public class SrvltLogger extends HttpServlet
 
                 JAXBContext jaxbContext = JAXBContext.newInstance(Errors.class);
                 Marshaller marshaller = jaxbContext.createMarshaller();
-                try (StringWriter stringWriterMarshaller = new StringWriter();)
-                {
+                try (StringWriter stringWriterMarshaller = new StringWriter();) {
                     marshaller.marshal(errors, stringWriterMarshaller);
                     return stringWriterMarshaller.toString();
                 }
@@ -119,46 +105,47 @@ public class SrvltLogger extends HttpServlet
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      * <p>
+     *
      * @param request  servlet request
      * @param response servlet response
-     * <p>
+     *                 <p>
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
     /**
      * Handles the HTTP <code>POST</code> method.
      * <p>
+     *
      * @param request  servlet request
      * @param response servlet response
-     * <p>
+     *                 <p>
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
     /**
      * Returns a short description of the servlet.
      * <p>
+     *
      * @return a String containing servlet description
      */
     @Override
-    public String getServletInfo()
-    {
+    public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
